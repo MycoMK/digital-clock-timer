6ca6480463d0b16d8d25c355da128f8e2ecb4e06
MICHAEL KINGS <kmichael381@gmail.com>
2021-01-21 17:59:27 -0800

    digital-countdown-bd

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const minsEl = document.getElementById("mins")
 const secondsEl = document.getElementById("seconds")
 
 
-const newYears = "1 Jan 2012";
+const newYears = "08 June 2021";
 
 function countdown(){
 
@@ -17,17 +17,20 @@ function countdown(){
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24)
-    
     const hours = Math.floor(totalSeconds / 3600 ) % 24;
-    
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-
+    const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds ) % 60;
 
-    const daysEl.innerHTML = days;
-    const hoursEl.innerHTML = hours;
-    const minsEl.innerHTML = mins;
-    const secondsEl.innerHTML = seconds;
+
+
+     daysEl.innerHTML = formatTime(days);
+     hoursEl.innerHTML = formatTime(hours);
+     minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+    function formatTime(time){
+        return time < 10 ? (`0${time}`) : time;
+    }
 }
 // initial call
 countdown();
